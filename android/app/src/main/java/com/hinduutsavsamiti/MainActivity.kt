e1b7d95333528e078188e45e8fcd45ade6aa4440
bhaviktalpada <bhaviktalpada@gmail.com>
2025-11-03 21:04:08 +0530

    Switch android and iOS package name

--- android/app/src/main/java/com/hinduutsavsamiti/MainActivity.kt
+++ android/app/src/main/java/com/hinduutsavsamiti/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hinduutsavsamitiapp
+package com.hinduutsavsamiti
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/hinduutsavsamiti/MainApplication.kt
+++ android/app/src/main/java/com/hinduutsavsamiti/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.hinduutsavsamitiapp
+package com.hinduutsavsamiti
 
 import android.app.Application
 import com.facebook.react.PackageList
