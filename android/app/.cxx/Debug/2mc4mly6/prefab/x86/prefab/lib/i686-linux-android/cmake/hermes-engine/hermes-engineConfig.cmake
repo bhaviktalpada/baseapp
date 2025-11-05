69ab6b0d035c5a4716ce0236b9ad551b7122de01
bhaviktalpada <bhaviktalpada@gmail.com>
2025-11-05 20:47:46 +0530

    basic changes

--- android/app/.cxx/Debug/2mc4mly6/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++ android/app/.cxx/Debug/2mc4mly6/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -1,8 +1,8 @@
 if(NOT TARGET hermes-engine::hermesvm)
 add_library(hermes-engine::hermesvm SHARED IMPORTED)
 set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "/Users/Mackbook/.gradle/caches/9.0.0/transforms/2b2a34f399e6566a4bc7f9555d03a28a/transformed/hermes-android-0.82.1-debug/prefab/modules/hermesvm/libs/android.x86/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/Mackbook/.gradle/caches/9.0.0/transforms/2b2a34f399e6566a4bc7f9555d03a28a/transformed/hermes-android-0.82.1-debug/prefab/modules/hermesvm/include"
+    IMPORTED_LOCATION "/Users/Mackbook/.gradle/caches/9.0.0/transforms/191d3e48612a3d0666169b3887611a25/transformed/jetified-hermes-android-0.82.1-debug/prefab/modules/hermesvm/libs/android.x86/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/Mackbook/.gradle/caches/9.0.0/transforms/191d3e48612a3d0666169b3887611a25/transformed/jetified-hermes-android-0.82.1-debug/prefab/modules/hermesvm/include"
     INTERFACE_LINK_LIBRARIES ""
 )
 endif()
